05ac768701bdfdba0c79f123e78c48b945d7d40e
terry12321 <tanzhenxuan76@gmail.com>
2025-02-15 17:43:41 +0800

    fix: move components folder out of apps folder, update layout to change file path

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import { Footer } from "./components/Footer";
-import { Header } from "./components/Header";
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
