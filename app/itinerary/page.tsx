c5dd14a4b6b28557a483226722230a5c60dc55d5
keith <limy0313@ntu.edu.sg>
2025-02-17 21:18:29 +0800

    initial setup for itinerary

---
+++ app/itinerary/page.tsx
@@ -0,0 +1,14 @@
+import { supabase } from "@/lib/supabase";
+
+export default function Itinerary() {
+  const setItinerary = async () => {
+    const { data, error } = await supabase.from("views").insert({
+      name: "random name",
+    });
+
+    if (data) console.log(data);
+    if (error) console.log(error);
+  };
+  setItinerary();
+  return <div>hello</div>;
+}
