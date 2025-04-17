d593514174d5666d83df830357978b877b6bef23
terry12321 <tanzhenxuan76@gmail.com>
2025-04-17 20:10:47 +0800

    feat: upgrade infinite loop carousel

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import HomeCarousel from "./HomeCarousel";
 import ImagePopup from "@/components/Modal"; // Adjust import based on your file structure
 import { useAuth } from "@/context/AuthContext";
+import TestCarousel from "@/components/TestCarousel/TestCarousel";
 
 export default function Home() {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
@@ -137,7 +138,10 @@ export default function Home() {
             />
           )}
         </div>
-        <HomeCarousel></HomeCarousel>
+        {/* <HomeCarousel></HomeCarousel> */}
+        <div className="w-full flex items-center justify-center mt-4">
+          <TestCarousel />
+        </div>
       </main>
     </div>
   );
