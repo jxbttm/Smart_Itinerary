e941ddfbfd16a38a36272fc3c7e4eb227dac3b24
gtengwei <gohtengwei1999@hotmail.com>
2025-04-21 02:10:20 +0800

    actions: update workflow for e2e testing

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -1,6 +1,6 @@
-// describe('Navigation', () => {
-//     it('should navigate to home page', () => {
-//       cy.visit('/')
-//       cy.get('h1', { timeout: 6000 }).should('be.visible').contains('Plan Your Dream Trip')
-//     })
-//   })
+describe('Navigation', () => {
+    it('should navigate to home page', () => {
+      cy.visit('/')
+      cy.get('h1', { timeout: 6000 }).should('be.visible').contains('Plan Your Dream Trip')
+    })
+  })
