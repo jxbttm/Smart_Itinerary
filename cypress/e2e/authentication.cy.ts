145abee189b7bc3405f1d647da963e25a8273700
jxbttm <su_jingxuan@hotmail.com>
2025-04-21 19:06:38 +0800

    test: test before each flow

--- cypress/e2e/authentication.cy.ts
+++ cypress/e2e/authentication.cy.ts
@@ -7,14 +7,13 @@ describe("Authenticated User", () => {
     // cy.setCookie(`sb-${projectRef}-auth-token.0`, "fake-access-token");
     // cy.setCookie(`sb-${projectRef}-auth-token.1`, "fake-refresh-token");
 
-    // cy.visit("/plan-itinerary", {
-    //   onBeforeLoad(win) {
-    //     win.localStorage.setItem("NEXT_PUBLIC_ENABLE_MOCK_AUTH", "true");
-    //   },
-    // });
+    cy.visit("/plan-itinerary", {
+      onBeforeLoad(win) {
+        win.localStorage.setItem("NEXT_PUBLIC_ENABLE_MOCK_AUTH", "true");
+      },
+    });
 
-    // cy.request("/api/test-login");
-      cy.visit("/plan-itinerary")
+    cy.request("/api/test-login");
   });
 
 
