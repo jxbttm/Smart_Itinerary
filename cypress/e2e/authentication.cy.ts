2676b34c1dfdbeb4ced502eb9c41c1ec1de32cf6
gtengwei <gohtengwei1999@hotmail.com>
2025-04-21 23:16:17 +0800

    actions:fix auth e2e test failing - add back build and removed useless code

--- cypress/e2e/authentication.cy.ts
+++ cypress/e2e/authentication.cy.ts
@@ -1,20 +1,7 @@
 
 describe("Authenticated User", () => {
 
-  // const projectRef = "ofisnfgjitykwijevgwi";
-
   beforeEach(() => {
-    // cy.setCookie(`sb-${projectRef}-auth-token.0`, "fake-access-token");
-    // cy.setCookie(`sb-${projectRef}-auth-token.1`, "fake-refresh-token");
-
-    // cy.visit("/plan-itinerary", {
-    //   onBeforeLoad(win) {
-    //     win.localStorage.setItem("NEXT_PUBLIC_ENABLE_MOCK_AUTH", "true");
-    //   },
-    // });
-
-    // cy.request("/api/test-login");
-
     cy.visit("/plan-itinerary");
   });
 
