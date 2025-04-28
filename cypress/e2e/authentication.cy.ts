a3e3f0b24dd9fbe6a8d2446d8f9df93a735f8711
jxbttm <su_jingxuan@hotmail.com>
2025-04-28 21:53:52 +0800

    fix: main head comment>

--- cypress/e2e/authentication.cy.ts
+++ cypress/e2e/authentication.cy.ts
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-// describe("Authenticated User", () => {
-//   beforeEach(() => {
-
-//     // MUST SETUP THE LOCAL STORAGE AND LOGIN API FOR THE Google SSO AUTH TO WORK
-//     // Add NEXT_PUBLIC_ENABLE_MOCK_AUTH=true in .env.local to enable mock auth
-//     cy.visit("/plan-itinerary", {
-//       onBeforeLoad(win) {
-//         win.localStorage.setItem("NEXT_PUBLIC_ENABLE_MOCK_AUTH", "true");
-//       },
-//     });
-//     cy.request("/api/test-login");
-
-
-//   });
-
-//   it("does NOT show Swal when user is authenticated", () => {
-    
-//     cy.url().should("include", "/plan-itinerary");
-
-//     // cy.contains("span", "Sign in with Google").should("not.exist");
-
-//     cy.get('input[type="checkbox"][name="my_preference"]').check({ force: true });
-
-//     cy.get(".swal2-container", { timeout: 8000 }).should("not.exist");
-//   });
-// });
-=======
 
 describe("Authenticated User", () => {
 
@@ -48,4 +20,3 @@ describe("Authenticated User", () => {
     // cy.get(".swal2-container", { timeout: 8000 }).should("not.exist");
   });
 });
->>>>>>> develop
