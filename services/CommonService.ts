ff359ec30663fbee113cd5e42628da79a638d43a
jxbttm <su_jingxuan@hotmail.com>
2025-05-03 10:03:51 +0800

    fix: common service type

--- services/CommonService.ts
+++ services/CommonService.ts
@@ -1,15 +1,16 @@
 import { BaseFetchStrategy } from "@/services/BaseFetchStrategy";
 import { TravelTypeFetchStrategy } from "@/services/TravelTypeFetchStrategy";
 import { FetchStrategyFactory } from "@/services/FetchStrategyFactory";
+import { FactoryType } from "@/types/FactoryType";
 
 export class CommonService {
 
-    static async fetchDataStrategy(dataType: string) {
+    static async fetchDataStrategy(factoryType: FactoryType) {
         const dataFetcher = new BaseFetchStrategy(
             new TravelTypeFetchStrategy(),
             new FetchStrategyFactory()
         );
-        dataFetcher.setStrategy(dataType)
+        dataFetcher.setStrategy(factoryType)
         return await dataFetcher.fetchData()
     };
 
